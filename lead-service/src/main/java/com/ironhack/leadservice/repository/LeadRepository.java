da89f2a27e7c33f9f648675e0e131e5bc8239f32
Anna Nodzyńska <7143565+neternefer@users.noreply.github.com>
2021-11-04 16:00:21 +0100

    Update LeadRepository.java

--- lead-service/src/main/java/com/ironhack/leadservice/repository/LeadRepository.java
+++ lead-service/src/main/java/com/ironhack/leadservice/repository/LeadRepository.java
@@ -10,4 +10,17 @@ import java.util.Optional;
 
 @Repository
 public interface LeadRepository extends JpaRepository<Lead, Long> {
+  
+    //Look up lead by id
+    Optional<Lead> findById(Long id);
+
+    //Report Lead by SalesRep
+    @Query(value = "SELECT sales_rep_report.sales_rep_name, COUNT(*) FROM lead_report " +
+            "RIGHT JOIN sales_rep_report ON lead_report.sales_rep_id " +
+            "GROUP BY sales_rep_report.sales_rep_name ORDER BY sales_rep_report.sales_rep_name", nativeQuery = true)
+    List<Object[]> findCountLeadByRepName();
+
+    //Show leads
+    @Query("SELECT l.id, l.name FROM Lead l")
+    List<Object[]> findAllLeads();
 }
