f7bdf54e3d5384fd28deb6b5a4a2db9a6dbc3e9a
ShaunRly <shaundr@hotmail.co.uk>
2021-11-05 20:54:54 +0000

    show accounts

--- menu-service/src/main/java/com/ironhack/menuservice/menus/MainMenu.java
+++ menu-service/src/main/java/com/ironhack/menuservice/menus/MainMenu.java
@@ -580,42 +580,30 @@ public class MainMenu implements Variables {
     public void showAccounts() {
         var allAccs = accountServiceProxy.getAccounts();
         System.out.println(colorMain + "\n╔════════════╦═══ " + colorMainBold + "Total Number Of Accounts: " + allAccs.size() + colorMain + " ═════════════╗" + reset);
-        System.out.printf("%-1s %-17s %-1s %-24s %-1s %-17s %-1s %-17s %-1s %-47s %-1s\n",
+        System.out.printf("%-1s %-17s %-1s %-50s %-1s\n",
                 colorMain + "║",
                 colorHeadlineBold + "ID",
                 colorMain + "║",
-                colorHeadlineBold + "Industry",
-                colorMain + "║",
-                colorHeadlineBold + "Employee Count",
-                colorMain + "║",
-                colorHeadlineBold + "City",
-                colorMain + "║",
-                colorHeadlineBold + "Country",
+                colorHeadlineBold + "Company name",
                 colorMain + "║");
-        System.out.printf("%-1s%-12s%-1s%-19s%-1s%-12s%-1s%-12s%-1s%-42s%-1s\n",
+        System.out.printf("%-1s%-12s%-1s%-45s%-1s\n",
                 colorMain + "╠",
                 "════════════",
                 "╬",
-                "═══════════════════",
-                "╬",
-                "════════════",
-                "╬",
-                "════════════",
-                "╬",
-                "══════════════════════════════════════════",
+                "═════════════════════════════════════════════",
                 "╣" + reset);
         for (int i = 0; i < allAccs.size(); i++) {
-            System.out.printf("%-1s%-12s%-1s%-19s%-1s%-12s%-1s%-12s%-1s%-42s%-1s\n",
+            String companyInfo = "";
+            if (allAccs.get(i).getContactList() == null){
+                companyInfo = "No Company associated with this Account";
+            } else {
+                companyInfo = contactServiceProxy.getContactById(allAccs.get(i).getContactList().get(0).getId()).getCompanyName();
+            }
+            System.out.printf("%-1s %-17s %-1s %-50s %-1s\n",
                     colorMain + "║",
                     colorTable + allAccs.get(i).getId(),
                     colorMain + "║",
-                    colorTable + allAccs.get(i).getIndustry(),
-                    colorMain + "║",
-                    colorTable + allAccs.get(i).getEmployeeCount(),
-                    colorMain + "║",
-                    colorTable + allAccs.get(i).getCity(),
-                    colorMain + "║",
-                    colorTable + allAccs.get(i).getCountry(),
+                    colorTable + companyInfo,
                     colorMain + "║" + reset);
         }
     }
